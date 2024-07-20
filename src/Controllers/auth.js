690fd260001902223a3faa50768da9c4e185d425
Azat <azatesser@gmail.com>
2024-07-20 15:03:37 +0300

    fix letter2

--- src/Controllers/auth.js
+++ src/Controllers/auth.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import User from "../Models/User.js";
+import User from "../models/User.js";
 import { secret_key } from "../config/env/index.js";
 
 async function register(req, res) {
