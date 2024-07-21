8eb841c0bdb12a8bb1156eedec45098ef5224b9f
Azat <azatesser@gmail.com>
2024-07-21 14:44:36 +0300

    refactor: Add taxRate field to Product model

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,16 +1,10 @@
 import mongoose from "mongoose";
 const productScheme = new mongoose.Schema({
     name: {type: String, required: true},
-    amount: {type: Number, required: true},
     unit: {type: String, required: true},
+    taxRate: {type: Number, required: true, default: 0, enum : [0, 1, 10, 20]},
     price: {type: Number, required: true, default: 0},
     priceWithTax: {type: Number, required: true, default: 0},
-    discountRate: {type: Number, required: true, default: 0},   
-    discountAmount: {type: Number, required: true, default: 0},
-    taxRate: {type: Number, required: true, default: 0, enum : [0, 1, 10, 20]},
-    taxAmount: {type: Number, required: true, default: 0},
-    total: {type: Number, required: true, default: 0},
-    totalWithTax: {type: Number, required: true, default: 0},
     createdAt: {type: Date, default: Date.now},
     updatedAt: {type: Date, default: Date.now},
 });
